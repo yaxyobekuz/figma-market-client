c3bd3e9643e3f0a3b572bcf39fd345941422e1db
yaxyobekuz <pubgn9642@gmail.com>
2025-12-01 21:08:46 +0500

    chore: add configuration files for ESLint, PostCSS, Tailwind CSS, and Next.js

---
+++ next.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.digitaloceanspaces.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
