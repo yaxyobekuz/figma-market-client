723bc074d435a353db0c4a14e9ff435cbcd218d0
yaxyobekuz <pubgn9642@gmail.com>
2025-12-09 21:44:07 +0500

    feat: refactor DesignItem component to use dynamic thumbnail and title props

--- src/components/ui/DesignItem.jsx
+++ src/components/ui/DesignItem.jsx
@@ -1,43 +1,56 @@
 // Next.js
+import Link from "next/link";
 import Image from "next/image";
 
-// Example design image
-import designImg from "@/assets/images/design.webp";
+// Api
+import { getApiImageUrl } from "@/api";
 
 // Icons
 import { Eye, Heart } from "lucide-react";
+import { cn } from "@/lib/utils";
 
-const DesignItem = ({ design, className, showAuthor = true, ...props }) => {
+const DesignItem = ({ className, _id: id, title, viewsCount, thumbnail }) => {
+  const thumbnailUrl = getApiImageUrl(thumbnail?.path);
+
   return (
-    <li className="">
-      <div className="relative">
+    <li className={cn("relative group pb-1.5 rounded-3xl", className)}>
+      {/* Top */}
+      <div className="relative w-full h-auto aspect-[4/3] bg-gray-100 overflow-hidden mb-3.5 rounded-3xl">
         {/* Image */}
         <Image
-          src={designImg}
-          alt="Design example image"
-          className="w-full h-auto aspect-[4/3] object-cover bg-gray-50 rounded-xl mb-3.5"
+          width={400}
+          height={300}
+          src={thumbnailUrl}
+          alt={title || "Design"}
+          className="size-full object-cover transition-transform duration-500 group-hover:scale-105"
         />
 
         {/* Toggle Like button */}
-        <button className="absolute top-3.5 right-3.5 bg-white text-gray-600 p-2.5 rounded-full">
+        <button className="absolute top-3.5 right-3.5 z-10 bg-white text-gray-600 p-2.5 rounded-full hover:text-violet-500 transition-colors">
           <Heart strokeWidth={1.5} size={20} />
         </button>
       </div>
 
+      {/* Main */}
       <div className="flex items-center justify-between px-1.5">
         {/* Title */}
-        <h3 className="line-clamp-1 font-medium">
-          Design name #{Math.floor(Math.random() * 100)}
+        <h3 className="line-clamp-1 font-medium transition-colors duration-200 group-hover:text-violet-500">
+          {title}
         </h3>
 
         {/* Views */}
         <div className="flex items-center gap-1.5 shrink-0 text-gray-500">
           <Eye strokeWidth={1.5} size={20} />
-          <span className="text-sm">
-            {Math.floor(Math.random() * 5000).toLocaleString()}
-          </span>
+          <span className="text-sm">{viewsCount || 0}</span>
         </div>
       </div>
+
+      {/* Link */}
+      <Link
+        href={`/designs/${id}`}
+        aria-label={`View details for ${title}`}
+        className="absolute inset-0 size-full rounded-3xl"
+      />
     </li>
   );
 };
